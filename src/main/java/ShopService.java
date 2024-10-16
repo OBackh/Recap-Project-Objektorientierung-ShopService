80c84fe4465efec1d5b2e8b554ac33ba3f086ea3
Ole Backhaus <ole.backhaus@gmx.de>
2024-10-16 18:28:07 +0200

    add ProductNotFoundException and adjust the addOrder method

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -10,13 +10,11 @@ public class ShopService {
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
-            if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
-            }
+            Product productToOrder = productRepo.getProductById(productId)
+                    .orElseThrow(() -> new ProductNotFoundException("Produkt mit der ID " + productId + " existiert nicht!"));
+
             products.add(productToOrder);
-        }
+            }
 
         Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING);
 

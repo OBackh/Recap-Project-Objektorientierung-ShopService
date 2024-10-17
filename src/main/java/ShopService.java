9f714628511d68fdaf3a2126b0df25b2eb603f60
Ole Backhaus <ole.backhaus@gmx.de>
2024-10-17 11:14:53 +0200

    add updateOrder in shopService and implement lombok in pom

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -24,4 +25,27 @@ public class ShopService {
     public List<Order> listAllOrdersByStatus(OrderStatus status){
         return orderRepo.getOrders().stream().filter(order -> order.status().equals(status)).collect(Collectors.toList());
     }
+
+    public Optional<Order> updateOrder(String orderId, OrderStatus newStatus) {
+        // Search Order by orderId
+        Order existingOrder = orderRepo.getOrders().stream()
+                .filter(order -> order.id().equals(orderId))
+                .findFirst()
+                .orElse(null);
+        // Check if order exists
+        if (existingOrder == null ) {
+            System.out.println("Keine Bestellung mit der ID " + orderId + " gefunden");
+            return Optional.empty();
+        }
+        // Update the status of the order
+        Order updatedOrder = existingOrder.withStatus(newStatus);
+        //Optional: save the new status of the order if the method addOrder saves the status
+        orderRepo.addOrder(updatedOrder);
+        // return updated order
+        return Optional.of(updatedOrder);
+
+    }
+
+
+
 }
